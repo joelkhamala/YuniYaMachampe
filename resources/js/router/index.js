7788e22ed46a68fb8a6cc5a377574ef6a4d8eae8
joelkhamala <jwanyoa@gmail.com>
2023-01-06 12:06:11 +0300

    Landing page Added

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -2,8 +2,6 @@ import { createWebHistory, createRouter } from 'vue-router'
 import store from '@/store'
 
 /* Guest Component */
-const Login = () => import('@/components/Login.vue')
-const Register = () => import('@/components/Register.vue')
 const Home = () => import('@/components/Home.vue')
 /* Guest Component */
 
@@ -13,50 +11,10 @@ const DashboardLayout = () => import('@/components/layouts/Default.vue')
 
 /* Authenticated Component */
 const Dashboard = () => import('@/components/Dashboard.vue')
-const AddCategory = () => import('@/components/categories/AddCategory.vue')
-const ViewCategory = () => import('@/components/categories/ViewCategory.vue')
-const EditCategory = () => import('@/components/categories/EditCategory.vue')
-
-const AddProduct = () => import('@/components/products/AddProducts.vue')
-const ViewProduct = () => import('@/components/products/ViewProducts.vue')
-const EditProduct = () => import('@/components/products/EditProduct.vue')
-
-const AddSupplier = () => import('@/components/suppliers/AddSuppliers.vue')
-const ViewSupplier = () => import('@/components/suppliers/ViewSuppliers.vue')
-const EditSupplier = () => import('@/components/suppliers/EditSupplier.vue')
-
-const AddCustomer = () => import('@/components/customers/AddCustomers.vue')
-const ViewCustomer = () => import('@/components/customers/ViewCustomers.vue')
-// const EditCustomer = () => import('@/components/customers/EditCustomer.vue')
-
-const AddOrder = () => import('@/components/orders/AddOrders.vue')
-const ViewOrder = () => import('@/components/orders/ViewOrders.vue')
-const EditOrder = () => import('@/components/orders/EditOrder.vue')
-
-const AddSales = () => import('@/components/sales/AddSales.vue')
-const ViewSales = () => import('@/components/sales/ViewSales.vue')
 /* Authenticated Component */
 
 
 const routes = [
-    {
-        name: "login",
-        path: "/login",
-        component: Login,
-        meta: {
-            middleware: "guest",
-            title: `Login`
-        }
-    },
-    {
-        name: "register",
-        path: "/register",
-        component: Register,
-        meta: {
-            middleware: "guest",
-            title: `Register`
-        }
-    },
     {
         name: "home",
         path: "/",
@@ -79,134 +37,6 @@ const routes = [
                 component: Dashboard,
                 meta: {
                     title: `Dashboard`
-                }
-            },
-            {
-                name: "addcategory",
-                path: '/category/addcat',
-                component: AddCategory,
-                meta: {
-                    title: `AddCategory`
-                }
-            },
-            {
-                name: "viewcategories",
-                path: '/category/viewcat',
-                component: ViewCategory,
-                meta: {
-                    title: `ViewCategory`
-                }
-            },
-            {
-                name: "editcategory",
-                path: '/category/catedit/:id',
-                component: EditCategory,
-                meta: {
-                    title: `EditCategory`
-                }
-            },
-            {
-                name: "addproduct",
-                path: '/products/addproduct',
-                component: AddProduct,
-                meta: {
-                    title: `AddProduct`
-                }
-            },
-            {
-                name: "viewproducts",
-                path: '/products/viewproduct',
-                component: ViewProduct,
-                meta: {
-                    title: `ViewProduct`
-                }
-            },
-            {
-                name: "editproduct",
-                path: '/products/productedit/:id',
-                component: EditProduct,
-                meta: {
-                    title: `EditProduct`
-                }
-            },
-            {
-                name: "addsupplier",
-                path: '/suppliers/addsupplier',
-                component: AddSupplier,
-                meta: {
-                    title: `AddSupplier`
-                }
-            },
-            {
-                name: "viewsuppliers",
-                path: '/suppliers/viewsupplier',
-                component: ViewSupplier,
-                meta: {
-                    title: `ViewSupplier`
-                }
-            },
-            {
-                name: "editsupplier",
-                path: '/suppliers/supplieredit/:id',
-                component: EditSupplier,
-                meta: {
-                    title: `EditSupplier`
-                }
-            },
-            {
-                name: "addcustomer",
-                path: '/customers/addcustomer',
-                component: AddCustomer,
-                meta: {
-                    title: `AddCustomer`
-                }
-            },
-            {
-                name: "viewcustomers",
-                path: '/customers/viewcustomers',
-                component: ViewCustomer,
-                meta: {
-                    title: `ViewCustomer`
-                }
-            },
-            {
-                name: "addorder",
-                path: '/orders/addorder',
-                component: AddOrder,
-                meta: {
-                    title: `AddOrder`
-                }
-            },
-            {
-                name: "vieworders",
-                path: '/suppliers/vieworder',
-                component: ViewOrder,
-                meta: {
-                    title: `ViewOrder`
-                }
-            },
-            {
-                name: "editorder",
-                path: '/suppliers/editorder/:id',
-                component: EditOrder,
-                meta: {
-                    title: `EditOrder`
-                }
-            },
-            {
-                name: 'addsales',
-                path: '/sales/addsales',
-                component: AddSales,
-                meta:{
-                    title: `AddSales`
-                }
-            },
-            {
-                name: "viewsales",
-                path: '/sales/viewsales',
-                component: ViewSales,
-                meta: {
-                    title: `ViewSales`
                 }
             },
         ]
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,31 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <title>Yuni Ya Machampe - Education is the Destiny</title>
+    <!-- META TAGS -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Education master is one of the best educational html template, it's suitable for all education websites like university,college,school,online education,tution center,distance education,computer education">
+    <meta name="keyword" content="education html template, university template, college template, school template, online education template, tution center template">
+    <!-- FAV ICON(BROWSER TAB ICON) -->
+    <link rel="shortcut icon" href="images/fav.ico" type="image/x-icon">
+    <!-- GOOGLE FONT -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700%7CJosefin+Sans:600,700" rel="stylesheet">
+    <!-- FONTAWESOME ICONS -->
+    <link rel="stylesheet" href="{{URL::to('css/font-awesome.min.css')}}">
+    <!-- ALL CSS FILES -->
+    <link href="{{URL::to('css/materialize.css')}}" rel="stylesheet">
+    <link href="{{URL::to('css/bootstrap.css')}}" rel="stylesheet" />
+    <link href="{{URL::to('css/style.css')}}" rel="stylesheet" />
+    <!-- RESPONSIVE.CSS ONLY FOR MOBILE AND TABLET VIEWS -->
+    <link href="{{URL::to('css/style-mob.css')}}" rel="stylesheet" />
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+	{{URL::to('<script src="js/html5shiv.js"></script>')}}
+	{{URL::to('<script src="js/respond.min.js"></script>')}}
+	<![endif]-->
         @vite(['resources/js/app.js'])
     </head>
     <body>
@@ -18,5 +36,10 @@
         </div>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+        <!--Import jQuery before materialize.js-->
+        <script src="{{URL::to('js/main.min.js')}}"></script>
+        <script src="{{URL::to('js/bootstrap.min.js')}}"></script>
+        <script src="{{URL::to('js/materialize.min.js')}}"></script>
+        <script src="{{URL::to('js/custom.js')}}"></script>
     </body>
 </html>
